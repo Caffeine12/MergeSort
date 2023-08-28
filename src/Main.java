faab8a3a2c89e62dac05b895635bf2b4b3ca524d
Caffeine12 <jubairku12@gmail.com>
2023-08-28 22:27:32 +0200

    Recursive method applied in deviding the array

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,37 @@
 import java.util.Arrays;
 
 public class Main {
+    public void sort(int[] sortArr)
+    {
+        int length=sortArr.length, mid=0;
+
+        System.out.println(Arrays.toString(sortArr));
+
+        if(length>2)
+        {
+            if(length%2==0)
+            mid=length/2;
+            else
+            mid=(length+1)/2;        
+            int sortHalfOne[]=Arrays.copyOfRange(sortArr, 0, mid);
+            int sortHalfTwo[]=Arrays.copyOfRange(sortArr, mid, length);
+            System.out.println(Arrays.toString(sortHalfOne));
+            System.out.println(Arrays.toString(sortHalfTwo));
+            Main obj=new Main();
+            obj.sort(sortHalfOne);
+            obj.sort(sortHalfTwo);
+
+        }
+        
+        
+        
+
+
+    }
     public static void main(String[] args) throws Exception {
         int sortArr[]={15, 10, 5, 8, 9, 4, 12};
-
-        int n=sortArr.length, l=0;
+        Main ob=new Main();
+        ob.sort(sortArr);
         
-        if(n%2==0)
-        l=n/2;
-        else
-        l=(n+1)/2;
-
-        int sortHalfOne[]=Arrays.copyOfRange(sortArr, 0, l);
-        int sortHalfTwo[]=Arrays.copyOfRange(sortArr, l, n);
-
-        System.out.println(Arrays.toString(sortHalfOne));
-        System.out.println(Arrays.toString(sortHalfTwo));
     }
 }
