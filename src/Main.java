95df3d8540bf887a47d0ec6ff31d3e1eebcd7e6c
Caffeine12 <jubairku12@gmail.com>
2023-09-08 21:29:10 +0200

    Merge Sort Implemented-Successful

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,91 @@
 import java.util.Arrays;
 
 public class Main {
-    public void sort(int[] sortArr)
+
+    public void merge(int[] sortArr,int start, int mid, int end)
     {
-        int length=sortArr.length, mid=0;
+        //Size to two subarrays
+        int sub1=mid-start+1;
+        int sub2=end-mid;
 
-        System.out.println(Arrays.toString(sortArr));
+        //Create temp arrays
+        int leftArray[]=new int[sub1];
+        int rightArray[]= new int[sub2];
 
-        if(length>2)
+        for (int i=0;i<sub1;i++)
         {
-            if(length%2==0)
-            mid=length/2;
+            leftArray[i]=sortArr[start+i];
+        }
+        for (int j=0;j<sub2;j++)
+        {
+            rightArray[j]=sortArr[mid+1+j];
+        }
+        //Merge the temp arrays
+        //initial indexes of first and second subarrays
+        int i=0, j=0;
+
+        //Initial index of merged subarray array
+        int k=start;
+        while(i<sub1 && j<sub2)
+        {
+            if(leftArray[i]<rightArray[j])
+            {
+                sortArr[k]=leftArray[i];
+                i++;
+            }
             else
-            mid=(length+1)/2;        
-            int sortHalfOne[]=Arrays.copyOfRange(sortArr, 0, mid);
-            int sortHalfTwo[]=Arrays.copyOfRange(sortArr, mid, length);
-            System.out.println(Arrays.toString(sortHalfOne));
-            System.out.println(Arrays.toString(sortHalfTwo));
-            Main obj=new Main();
-            obj.sort(sortHalfOne);
-            obj.sort(sortHalfTwo);
+            {
+                sortArr[k]=rightArray[j];
+                j++;
+            }
+            k++;
+        }
+        
+        //Copying rest of the values
+        while(i<sub1)
+        {
+            sortArr[k]=leftArray[i];
+            i++;
+            k++;
+
+        }
+        while(j<sub2)
+        {
+            sortArr[k]=rightArray[j];
+            j++;
+            k++;
 
         }
         
-        
-        
-
-
     }
-    public static void main(String[] args) throws Exception {
-        int sortArr[]={15, 10, 5, 8, 9, 4, 12};
-        Main ob=new Main();
-        ob.sort(sortArr);
+
+    public void sort(int[] sortArr, int start, int end)
+    {
+        if(start<end)
+        {
+            //Finding mid point
+            int mid=(start+end)/2;
+
+            //Devide the array in two arrays
+            sort(sortArr,start,mid);            
+            sort(sortArr,mid+1,end);    
+            
+            //merge the devided arrays
+            merge(sortArr,start,mid,end);
+        }
         
+    }
+    public static void main(String[] args) throws Exception 
+    {
+
+        int givenArr[]={15, 10, 5, 8, 9, 4, 12};
+        System.out.println("Given Array: "+ Arrays.toString(givenArr));
+
+        //Calling sort method
+        Main ob=new Main();
+        ob.sort(givenArr,0,givenArr.length-1);
+        System.out.println("Sorted Array: "+ Arrays.toString(givenArr));
+
+        System.out.print("End");        
     }
 }
